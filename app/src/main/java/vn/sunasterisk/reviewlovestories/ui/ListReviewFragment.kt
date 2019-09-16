2d453685f2f23a03bda77a422c9d601886f4324a
anhdaono211 <anhdaono211@gmail.com>
2019-09-16 11:15:03 +0700

    16932_home_ui

---
+++ app/src/main/java/vn/sunasterisk/reviewlovestories/ui/ListReviewFragment.kt
@@ -0,0 +1,19 @@
+package vn.sunasterisk.reviewlovestories.ui
+
+import vn.sunasterisk.reviewlovestories.R
+import vn.sunasterisk.reviewlovestories.base.BaseFragment
+
+class ListReviewFragment : BaseFragment() {
+    override val getContentViewId = R.layout.fragment_list_review
+    override fun setUpViewModelAndDataBinding() {
+
+    }
+
+    override fun observeData() {
+
+    }
+
+    override fun handleEvents() {
+
+    }
+}
---
+++ app/src/main/java/vn/sunasterisk/reviewlovestories/ui/ReviewFragment.kt
@@ -0,0 +1,19 @@
+package vn.sunasterisk.reviewlovestories.ui
+
+import vn.sunasterisk.reviewlovestories.R
+import vn.sunasterisk.reviewlovestories.base.BaseFragment
+
+class ReviewFragment : BaseFragment() {
+    override val getContentViewId = R.layout.fragment_review
+    override fun setUpViewModelAndDataBinding() {
+
+    }
+
+    override fun observeData() {
+
+    }
+
+    override fun handleEvents() {
+
+    }
+}
